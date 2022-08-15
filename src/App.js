1fbd657e196fd359e9d41168f28bb3206647afd0
thehouseplant <sean.collins@outlook.com>
2022-08-14 19:34:39 -0700

    Add basic navigation styling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const App = () => {
     <main>
       <h1>Reacker News</h1>
       <nav>
-        <Link to="/new">New</Link>
-        <Link to="/show">Show</Link>
-        <Link to="/ask">Ask</Link>
+        <Link to="/new">New</Link> | {" "}
+        <Link to="/show">Show</Link> | {" "}
+        <Link to="/ask">Ask</Link> | {" "}
         <Link to="/jobs">Jobs</Link>
       </nav>
     </main>
