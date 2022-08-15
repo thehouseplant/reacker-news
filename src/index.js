46b933f13e70a6d745cf3b01afe163f104e1629c
thehouseplant <sean.collins@outlook.com>
2022-08-14 19:34:50 -0700

    Add routing for each of the paths

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
+import Ask from './routes/Ask';
+import Jobs from './routes/Jobs';
+import New from './routes/New';
+import Show from './routes/Show';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
@@ -10,6 +14,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/ask" element={<Ask />} />
+      <Route path="/jobs" element={<Jobs />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/show" element={<Show />} />
+    </Routes>
   </BrowserRouter>
 );
